47c1d4268e822c00f2b433d07287a6320eb7e833
pyculiar <pyculiar@pm.me>
2024-11-23 23:25:08 +0200

    some minor refactoring

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 def char_count(text: str) -> dict[str, int]:
-    text = text.lower()
     chars_counts = {}
-    for char in text:
+    for char in text.lower():
         if char.isalpha():
-            if char not in chars_counts.keys():
-                chars_counts[char] = 1
-            else:
-                chars_counts[char] += 1
-    chars_counts = sorted(chars_counts.items(), key=lambda x: x[1], reverse=True)
-    return dict(chars_counts)
+            chars_counts[char] = chars_counts.get(char, 0) + 1
+    return dict(sorted(chars_counts.items(), key=lambda x: x[0]))
 
 def word_count(text: str) -> int:
-    words = text.split()
-    return len(words)
+    return len(text.split())
 
 def main():
     book_path = "books/frankenstein.txt"
-    print(f"### Report on {book_path} ###")
     with open(book_path) as f:
         file_contents = f.read()
     wordcount = word_count(file_contents)
-    print(f"{wordcount} words found in the book")
     chars_counts = char_count(file_contents)
+    print(f"### Report on {book_path} ###")
+    print(f"{wordcount} words found in the book")
     for k, v in chars_counts.items():
         print(f"The '{k}' character was found {v} times")
     print("### End of the report ###")
